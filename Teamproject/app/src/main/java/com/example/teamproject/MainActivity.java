6fca36705d017fc2a149cda18e477e166134a44b
4nathon <gotjd344@naver.com>
2020-12-03 15:28:37 +0900

    Revert "2020/12/03"
    This reverts commit 3fe2913fa3fe232b86137646b80a7ba92c3a7732.

--- Teamproject/app/src/main/java/com/example/teamproject/MainActivity.java
+++ Teamproject/app/src/main/java/com/example/teamproject/MainActivity.java
@@ -1,22 +1,15 @@
 package com.example.teamproject;
 
-import android.Manifest;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import org.w3c.dom.Document;
@@ -57,30 +50,20 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 
+
+
+
 public class MainActivity extends AppCompatActivity {
-    TextView textView, textViewDetail, textViewDetail2, textViewDetail3;
+    TextView textView;
+
     HashMap<String, Double> la = new HashMap<String, Double>();//위도
     HashMap<String, Double> lo = new HashMap<String ,Double>();//경도
-    String result, result2, result3, result4;
 
-    private static final int MULTIPLE_PERMISSION = 10235;
-    private String[] PERMISSIONS = {
-            Manifest.permission.SEND_SMS,
-            Manifest.permission.RECEIVE_SMS,
-            Manifest.permission.READ_PHONE_STATE
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (!hasPermissions(this, PERMISSIONS)) {
-            ActivityCompat.requestPermissions(this, PERMISSIONS, MULTIPLE_PERMISSION);
-        } else {
-            /*..권한이 있는경우 실행할 코드....*/
-            System.out.println("권한이 있음.");
-        }
 
         //공항 좌표값 할당
         la.put("AMS",4.7674241);
@@ -149,19 +132,16 @@ public class MainActivity extends AppCompatActivity {
         la.put("CJU",33.5068  );
         lo.put("CJU",126.4892);
 
-        //final SwipeRefreshLayout swipeRefreshLayout=(SwipeRefreshLayout)findViewById(R.id.swip_layout);
+        final SwipeRefreshLayout swipeRefreshLayout=(SwipeRefreshLayout)findViewById(R.id.swip_layout);
         textView = (TextView)findViewById(R.id.textView);
-        textViewDetail = (TextView)findViewById(R.id.textView2);
-        textViewDetail2 = (TextView)findViewById(R.id.textView3);
-        textViewDetail3 = (TextView)findViewById(R.id.textView4);
 
-        /*swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 Toast.makeText(MainActivity.this, "새로고침 되었습니다.", Toast.LENGTH_SHORT).show();
                 swipeRefreshLayout.setRefreshing(false);
             }
-        });*/
+        });
 
         //String api = "http://openapi.airport.kr/openapi/service/StatusOfPassengerWeahter/getPassengerArrivalsW?ServiceKey=URTjt6uEUH9SIYq1H3zFtGNg65d2oeLiJGJPk0t1ZcEtUJIw4x4BjRSXIxtM8ZZLI9xiQswQyiD0Us3lNCAaHQ%3D%3D&pageNo=1&numOfRows=10&from_time=0000&to_time=2400&airport=HKG&airline=KE&lang=K";
         String api = "http://openapi.airport.kr/openapi/service/StatusOfPassengerWeahter/getPassengerDeparturesW?ServiceKey=URTjt6uEUH9SIYq1H3zFtGNg65d2oeLiJGJPk0t1ZcEtUJIw4x4BjRSXIxtM8ZZLI9xiQswQyiD0Us3lNCAaHQ%3D%3D";
@@ -174,8 +154,9 @@ public class MainActivity extends AppCompatActivity {
         DownloadWebpageTask task = new DownloadWebpageTask();
 
         task.execute(api);
-
     }
+
+
 
     private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
         @Override
@@ -192,8 +173,6 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) { // ver1
             super.onPostExecute(result);
-
-            Button btn_prev, btn_next;
 
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             factory.setNamespaceAware(true);
@@ -228,7 +207,7 @@ public class MainActivity extends AppCompatActivity {
             } catch (XPathExpressionException e) {
                 e.printStackTrace();
             }
-            /*String result2 = null;
+            String result2 = null;
             for (int i = 0; i < nodeList.getLength(); i++) {
                 NodeList child = nodeList.item(i).getChildNodes();
                 for (int j = 0; j < child.getLength(); j++) {
@@ -238,171 +217,13 @@ public class MainActivity extends AppCompatActivity {
                     result2 += node.getTextContent();
                     result2 += "\n";
                 }
+                result2 += "\n";
             }
+
+
+
 
             textView.setText(result2);
-            */
-            result2 = "";
-
-            try {
-                expr = xpath.compile("flightId");
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            Node node = null;
-            try {
-                node = (Node) expr.evaluate(nodeList.item(0), XPathConstants.NODE);
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            result2 += node.getTextContent();
-            result2 += "\n";
-            try {
-                expr = xpath.compile("airline");
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            try {
-                node = (Node) expr.evaluate(nodeList.item(0), XPathConstants.NODE);
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            result2 += node.getTextContent();
-            result2 += "\n";
-
-            textView.setText(result2);
-
-            result3 = "";
-
-            try {
-                expr = xpath.compile("airport");
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            Node node2 = null;
-            try {
-                node2 = (Node) expr.evaluate(nodeList.item(0), XPathConstants.NODE);
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            result3 += "공항 : ";
-            result3 += node2.getTextContent();
-            result3 += "\n";
-            try {
-                expr = xpath.compile("scheduleDateTime");
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            try {
-                node2 = (Node) expr.evaluate(nodeList.item(0), XPathConstants.NODE);
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            result3 += "도착 예정 시간 : ";
-            result3 += node2.getTextContent().charAt(0);
-            result3 += node2.getTextContent().charAt(1);
-            result3 += "시 ";
-            result3 += node2.getTextContent().charAt(2);
-            result3 += node2.getTextContent().charAt(3);
-            result3 += "분";
-            result3 += "\n";
-            try {
-                expr = xpath.compile("estimatedDateTime");
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            try {
-                node2 = (Node) expr.evaluate(nodeList.item(0), XPathConstants.NODE);
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            result3 += "도착 변경 시간 : ";
-            result3 += node2.getTextContent().charAt(0);
-            result3 += node2.getTextContent().charAt(1);
-            result3 += "시 ";
-            result3 += node2.getTextContent().charAt(2);
-            result3 += node2.getTextContent().charAt(3);
-            result3 += "분";
-            result3 += "\n";
-
-            textViewDetail.setText(result3);
-
-            result4 = "";
-
-            try {
-                expr = xpath.compile("gatenumber");
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            Node node3 = null;
-            try {
-                node3 = (Node) expr.evaluate(nodeList.item(0), XPathConstants.NODE);
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            result4 += "탑승구 : ";
-            result4 += node3.getTextContent();
-            result4 += "\n";
-            /*try {
-                expr = xpath.compile("exitnumber");
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            try {
-                node3 = (Node) expr.evaluate(nodeList.item(0), XPathConstants.NODE);
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            result4 += "출구 : ";
-            result4 += node3.getTextContent();
-            result4 += "\n";*/
-            try {
-                expr = xpath.compile("remark");
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            try {
-                node3 = (Node) expr.evaluate(nodeList.item(0), XPathConstants.NODE);
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            result4 += "상태 : ";
-            result4 = node3.getTextContent();
-            result4 += "\n";
-
-            textViewDetail2.setText(result4);
-
-            String result5 = "";
-
-            try {
-                expr = xpath.compile("mintem");
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            Node node4 = null;
-            try {
-                node4 = (Node) expr.evaluate(nodeList.item(0), XPathConstants.NODE);
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            result5 += "최저 기온 : ";
-            result5 += node4.getTextContent();
-            result5 += "도\n";
-            try {
-                expr = xpath.compile("maxtem");
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            try {
-                node4 = (Node) expr.evaluate(nodeList.item(0), XPathConstants.NODE);
-            } catch (XPathExpressionException e) {
-                e.printStackTrace();
-            }
-            result5 += "최고 기온 : ";
-            result5 += node4.getTextContent();
-            result5 += "도\n";
-
-            textViewDetail3.setText(result5);
         }
 
         private String downloadUrl(String api) throws IOException {
@@ -450,13 +271,11 @@ public class MainActivity extends AppCompatActivity {
                 conn.disconnect();
             }
         }
-
     }// class DownloadWebpageTask
 
     public void showMap(View view) {
-//        Button b = (Button)findViewById(R.id.next);
-//        String code = b.getText().toString();
-        String code="AMS";
+        Button b = (Button)view;
+        String code = b.getText().toString();
         double longitude = lo.get(code);
         double latitude = la.get(code);
         int zoom = 10;
@@ -465,58 +284,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW, geo);
         startActivity(intent);
 
-    }
-
-    public void Messaging(View view){
-        Intent intent=new Intent(this, MainActivity2.class);
-        intent.putExtra("result2", result2);
-        intent.putExtra("result3", result3);
-        intent.putExtra("result4", result4);
-        startActivity(intent);
-    }
-
-    public static boolean hasPermissions(Context context, String... permissions) {
-        if (context != null && permissions != null) {
-            for (String permission : permissions) {
-                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    //권한 요청에 대한 결과 처리
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MULTIPLE_PERMISSION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    /*..권한이 있는경우 실행할 코드....*/
-                    System.out.println("권한이 있음.");
-                } else {
-                    // 하나라도 거부한다면.
-                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-                    alertDialog.setTitle("앱 권한");
-                    alertDialog.setMessage("해당 앱의 원할한 기능을 이용하시려면 애플리케이션 정보>권한> 에서 모든 권한을 허용해 주십시오");
-                    // 권한설정 클릭시 이벤트 발생
-                    alertDialog.setPositiveButton("권한설정",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
-                                    startActivity(intent);
-                                    dialog.cancel();
-                                }
-                            });
-                    //취소
-                    alertDialog.setNegativeButton("취소",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.cancel();
-                                }
-                            });
-                    alertDialog.show();
-                }
-                return;
-        }
     }
 }
